5fc19a744d21607ed2db76c443c2a0735f56911b
Fernando Vera <69645075+fershio20@users.noreply.github.com>
2024-12-03 20:50:36 -0300

    Adds main page, hero component and about component

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from "./components/Hero.jsx";
+import {About} from "./components/about.jsx";
+
+
+export const App = () => {
+    return (
+        <main className='relative min-h-screen w-screen overflow-x-hidden'>
+            <Hero />
+            <About />
+            <section className='z-0 min-h-screen bg-blue-500' />
+
+        </main>
+    )
+}
+
+export default App;
